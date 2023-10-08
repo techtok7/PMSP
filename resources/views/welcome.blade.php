241e1f34e096363efcfc73978fbff18e6837f0a6
JEEL RUPARELIYA <jeelrupareliya255@gmail.com>
2023-10-08 12:42:32 +0530

    UI

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,14 +14,16 @@
     <body>
         <header>
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a class="navbar-brand" href="#"><i class="material-icons">home</i> Home</a>
+                <a class="navbar-brand" href="http://127.0.0.1:8000/"><i class="material-icons">home</i> Home</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav ml-auto">
-                        <li class="nav-item"><a class="nav-link" href="#"><i class="material-icons"></i> Login</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#"><i class="material-icons"></i> Register</a></li>
+                        
+                        <a href="{{ route('login') }}">
+                        <li class="nav-item"><a class="nav-link" href="http://127.0.0.1:8000/login"><i class="material-icons"></i> Login</a></li>
+                        <li class="nav-item"><a class="nav-link" href="http://127.0.0.1:8000/register"><i class="material-icons"></i> Register</a></li>
                         
                     </ul>
                 </div>
